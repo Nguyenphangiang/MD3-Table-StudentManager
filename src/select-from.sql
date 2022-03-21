9f42549e7021d3e15ca5ce3a1e099a8959b7b43e
Nguyenphangiang <phancruel@gmail.com>
2022-03-21 15:00:25 +0700

    add update, sort by mark

--- src/select-from.sql
+++ src/select-from.sql
@@ -4,3 +4,11 @@ select * from subject where Credit < 10;
 select StudentID,StudentName,ClassName from student join class c on student.ClassID = c.ClassID where ClassName  = 'A1';
 select student.StudentID,StudentName,s.SubName,m.Mark from student join mark m on student.StudentID = m.StudentID join subject s on s.SubID = m.SubID
 where s.SubName = 'CF';
+
+select * from student where StudentName like 'h%';
+select * from class where month(StartDate)=12;
+select * from subject where Credit between 3 and 5;
+
+update student set ClassID = 2 where StudentName = 'Hung';
+
+select StudentName,SubName,Mark from student join subject s on student.Status = s.Status join mark m on s.SubID = m.SubID order by Mark DESC ;
